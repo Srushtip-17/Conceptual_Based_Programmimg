b480f464c552c921f303ca1acf93d28a8b981b10
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-02 09:37:39 +0530

    Add initial C++ demo program

---
+++ C++_Programming/Demo.cpp
@@ -0,0 +1,15 @@
+//this is editor
+//cd Desktop/PPa/Cpp_programming --this is for terminal
+
+#include <iostream>  //preprocessor <input output>
+using namespace std;   //namespace = binding things organized dara
+
+int main()
+{
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
+
+// g++ Demo.cpp -o Myexe
+//  .\Myexe
