7463e4f54b6106ac4c4ae2c225ac787b2502fa62
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-09 09:50:06 +0530

    Add files via upload

---
+++ C_Programming/StructureObjectArray.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+//Declaration
+struct Demo
+{
+    int i;      //4
+    float f;    //4
+};              //8  
+
+int main()
+{
+    struct  Demo Arr[4];
+    
+    printf("Size of Arr is %lu",sizeof(Arr));
+
+    Arr[0].i =11;
+    Arr[0].f =11.0f;
+    
+    Arr[1].i=21;
+    Arr[1].f=21.0f;
+
+    Arr[2].i =51;
+    Arr[2].f =51.0f;
+    
+    Arr[3].i=101;
+    Arr[3].f=101.0f;
+    
+    printf("%d\n",Arr[2].i);    //51
+    printf("%f\n",Arr[1].f);    //21
+
+    return 0;
+}
