77662c177b704bceb637118357aa0dff33107dae
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-09 09:50:28 +0530

    Add files via upload

---
+++ C_Programming/StructurePointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//Declaration
+struct Demo
+{
+    int i;      //4
+    int *ptr;   //8
+};              //12
+
+int main()
+{
+    struct Demo dobj;
+    
+    printf("Size of object is :%lu\n",sizeof(dobj));    
+    //either 8 or 16
+
+    return 0;
+}
